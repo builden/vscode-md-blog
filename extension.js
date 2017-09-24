a975f3bc9be2f0951272417145e3bca5cf2bbb5f
builden <billdeng@qq.com>
2017-09-24 20:21:46 +0800

    1. support checkbox 2. support emoji 3. support mermaid

---
+++ extension.js
@@ -0,0 +1,32 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+var vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "vscode-md-blog" is now active!');
+
+  return {
+    extendMarkdownIt(md) {
+      md.use(require('markdown-it-emoji'));
+      md.use(require('markdown-it-task-checkbox'));
+
+      const highlight = md.options.highlight;
+      md.options.highlight = (code, lang) => {
+        if (lang && lang.toLowerCase() === 'mermaid') {
+          return `<div class="mermaid">${code}</div>`;
+        }
+        return highlight(code, lang);
+      };
+      return md;
+    },
+  };
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+exports.deactivate = deactivate;
---
+++ mermaid-init.js
@@ -0,0 +1,5 @@
+const mermaidScript = [].filter.call(document.getElementsByTagName('script'), x =>
+  x.getAttribute('src').endsWith('mermaid.min.js')
+)[0];
+
+mermaidScript.onload = () => mermaid.initialize({ startOnLoad: true });
