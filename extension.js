bdc305df85d4a7e0336399ef1772d454b5ee2175
builden <billdeng@qq.com>
2017-09-25 17:29:02 +0800

    add twemoji

--- extension.js
+++ extension.js
@@ -1,17 +1,24 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
+const twemoji = require('twemoji');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vscode-md-blog" is now active!');
+  console.log(
+    'Congratulations, your extension "vscode-md-blog" is now active!'
+  );
 
   return {
     extendMarkdownIt(md) {
       md.use(require('markdown-it-emoji'));
+      md.renderer.rules.emoji = function(token, idx) {
+        return twemoji.parse(token[idx].content);
+      };
+
       md.use(require('markdown-it-task-checkbox'));
 
       const highlight = md.options.highlight;
@@ -22,7 +29,7 @@ function activate(context) {
         return highlight(code, lang);
       };
       return md;
-    },
+    }
   };
 }
 exports.activate = activate;
